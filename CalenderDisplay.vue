84625f75bff9343773a8db921b36bb5d9f6544c8
shreyashre16 <shreyachowta16@gmail.com>
2024-09-23 15:53:09 +0530

    Added calendar component using vue-cal

---
+++ CalenderDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mt-4">
+    <h2>Calendar</h2>
+    <vue-cal />
+  </div>
+</template>
+
+<script>
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
+
+export default {
+  components: {
+    VueCal,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
