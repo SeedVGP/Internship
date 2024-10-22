64daccfcb8ca10c4ce58b6f5d7338b35688322aa
shreyashre16 <shreyachowta16@gmail.com>
2024-10-22 12:39:48 +0530

    Implemented a counter that increments when the button is clicked.

---
+++ CounterComponent.vue
@@ -0,0 +1,23 @@
+<!-- CounterComponent.vue -->
+<template>
+    <div>
+      <p>{{ counter }}</p>
+      <button @click="incrementCounter">Increment</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+    methods: {
+      incrementCounter() {
+        this.counter++;
+      },
+    },
+  };
+  </script>
+  
