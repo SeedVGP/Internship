a4f8840140f04b005544123cf7ccf3d235e238cc
shreyashre16 <shreyachowta16@gmail.com>
2024-10-23 10:05:31 +0530

    added Display component to show current count value from parent

---
+++ DisplayComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Displays the current count value passed as a prop -->
+    <p>Count: {{ count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * Display component that shows the current count value passed from the parent component.
+   *
+   * @component Display
+   */
+  props: {
+    /**
+     * The current count value to be displayed.
+     *
+     * @prop {number} count - The count value passed from the parent component. It is required.
+     */
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
