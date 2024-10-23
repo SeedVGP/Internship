ac2a16104a3a51417a1123f7abb3315d0ed53263
shreyashre16 <shreyachowta16@gmail.com>
2024-10-23 10:03:50 +0530

    added CounterComp to emit increment event on button click

---
+++ CounterComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Button that triggers the increment event when clicked -->
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * Counter component that emits an `increment` event when the button is clicked.
+   *
+   * @component CounterComp
+   */
+  methods: {
+    /**
+     * Emits the `increment` event to notify the parent component that the count should be incremented.
+     *
+     * @method increment
+     * @emit increment - Event emitted when the button is clicked to increment the count.
+     */
+    increment() {
+      this.$emit('increment'); // Emit an event when incremented
+    },
+  },
+};
+</script>
